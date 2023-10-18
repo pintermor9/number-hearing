07d9b982e9c35635969ca00c2c3f4d54da75b6f8
pintermor9 <pintermor9@gmail.com>
2023-10-18 19:08:22 +0200

    v0.4

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var AUTOPLAY = true;
-var MAX_NUM = 999;
-var PLAY_ON_SPACE = true;
+const play_button = document.querySelector("#play");
+const play_button_spinner = document.querySelector("#play>span.spinner-border");
+const field_example = document.querySelector("#field-example");
+const inputs = document.querySelector(".inputs");
+const feedback = document.querySelector("#feedback");
+const check_button = document.querySelector("#check");
+const settings_elements = document.querySelectorAll("[data-setting]");
+
 var number;
 var number_audio;
-var play_button = document.getElementById("play");
-var play_button_spinner = document.querySelector("#play > span.spinner-border");
-var field_example = document.querySelector("#field-example");
-var inputs = document.querySelector(".inputs");
-var feedback = document.getElementById("feedback");
-var check_button = document.querySelector("button#check");
 
-var checked = false;
+var checked_input = false;
 
 const searchParams = new URLSearchParams(window.location.search);
 
+// this is temporary
 if (searchParams.has("redirectedToUpdated")) {
   alert(
     "A weboldal címe megváltozott. Automatikusan át lett irányítva. // The link to this site has changed. You have been redirected."
@@ -21,15 +21,67 @@ if (searchParams.has("redirectedToUpdated")) {
   window.location.replace("/number-hearing");
 }
 
+// ANCHOR - settings
+var settings = JSON.parse(localStorage.getItem("settings"));
+
+if (settings == null) {
+  settings = {};
+  readSettings();
+} else {
+  settings_elements.forEach((element) => {
+    if (element.type == "number") {
+      element.value = settings[element.id];
+    } else if (element.type == "select-one") {
+      element.value = settings[element.id];
+    } else if (element.type == "checkbox") {
+      element.checked = settings[element.id];
+    }
+  });
+}
+
+function readSettings() {
+  var old = JSON.parse(JSON.stringify(settings));
+  settings_elements.forEach((element) => {
+    if (element.type == "number") {
+      settings[element.id] = parseInt(element.value);
+    } else if (element.type == "select-one") {
+      settings[element.id] = element.value;
+    } else if (element.type == "checkbox") {
+      settings[element.id] = element.checked;
+    }
+  });
+  localStorage.setItem("settings", JSON.stringify(settings));
+
+  if (old.display_lang != settings.display_lang) reloadDisplayLanguage();
+  if (old.lang != settings.lang) generate_new();
+}
+
+function reloadDisplayLanguage() {
+  var req = new XMLHttpRequest();
+  req.open("get", "languages.json", false);
+  req.addEventListener("readystatechange", (event) => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      // TODO
+    }
+  });
+  req.send();
+}
+
+// ANCHOR - add event listeners
 document.addEventListener("keydown", (event) => {
   if (event.key == "Enter") {
-    if (checked) generate_new();
+    if (checked_input) generate_new();
     else check();
   }
 });
 
 check_button.addEventListener("click", check);
 
+settings_elements.forEach((element) =>
+  element.addEventListener("input", readSettings)
+);
+
+// ANCHOR - functions
 function get_field_by_index(i) {
   return inputs.children[i].firstElementChild;
 }
@@ -74,14 +126,14 @@ function field_keydown(event) {
     }
   } else if (event.code == "Space") {
     event.preventDefault();
-    if (PLAY_ON_SPACE) play();
+    if (settings.play_on_space) play();
   } else {
     event.preventDefault();
   }
 }
 
 function generate_new() {
-  number = Math.round(Math.random() * MAX_NUM);
+  number = Math.round(Math.random() * settings.max_num);
   console.log("current number: " + number);
 
   for (let i = inputs.children.length; i > 0; i--) {
@@ -98,7 +150,7 @@ function generate_new() {
 
   inputs.firstElementChild.firstElementChild.focus();
 
-  checked = false;
+  checked_input = false;
   check_button.disabled = false;
   play_button.disabled = true;
   play_button_spinner.hidden = false;
@@ -121,13 +173,21 @@ function check() {
     for (let i = 0; i < inputs.children.length; i++) {
       get_field_by_index(i).disabled = true;
     }
-    checked = true;
+    checked_input = true;
     check_button.disabled = true;
   }
 }
 
 function tts(text) {
-  number_audio = new Audio("https://utils.pintermor9.repl.co/tts?text=" + text);
+  audio_url = "https://utils.pintermor9.repl.co/tts";
+  audio_url +=
+    "?" +
+    new URLSearchParams([
+      ["text", text],
+      ["lang", settings.lang],
+    ]).toString();
+
+  number_audio = new Audio(audio_url);
 
   number_audio.addEventListener("error", () => {
     alert(
@@ -141,7 +201,7 @@ function tts(text) {
   number_audio.addEventListener("canplaythrough", () => {
     play_button.disabled = false;
     play_button_spinner.hidden = true;
-    if (AUTOPLAY) number_audio.play();
+    if (settings.autoplay) number_audio.play();
   });
 }
 
