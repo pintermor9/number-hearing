fce68bf1dce480edf5d6f7ecc4bfcfa0a22bddbe
pintermor9 <86710821+pintermor9@users.noreply.github.com>
2023-10-29 08:20:35 +0100

    start creating skeleton of script v2

--- script2.js
+++ script2.js
@@ -1,1 +1,34 @@
 // TODO rewrite
+
+// TODO allow preloading next number's audio
+
+const play_button = document.querySelector("#play");
+
+var next_number = {
+    number: null,
+    audio: null,
+    settings: null
+}
+var number = {
+    number: null, 
+    audio: null
+}
+var settings = {}
+
+function next() {
+    if (settings == next_number.settings) {
+        // set number= next_number
+        // create new next_number
+    } else {
+        // create both
+    }
+}
+
+function generate_number() {
+    // TODO add settings to allow zeros, doubles, etc.
+    return Math.round(Math.random() * settings.max_num);
+}
+
+function get_audio(number) {
+    
+}
